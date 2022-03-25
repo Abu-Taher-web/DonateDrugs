b1b52a2c5513850784bbbe861a069d4d188f1cbc
Taher <ataher622@gmail.com>
2022-03-26 00:09:38 +0600

    completed user login page functionality

--- Site1.Master.cs
+++ Site1.Master.cs
@@ -26,7 +26,7 @@ namespace DonateDrugs
 
         protected void userLogin_Click(object sender, EventArgs e)
         {
-
+            Response.Redirect("userLogin.aspx");
         }
 
         protected void signUp_Click(object sender, EventArgs e)
